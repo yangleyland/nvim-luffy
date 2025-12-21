d108ab1957c972afaa2bb257713f2241f926db62
Leyland Yang <yangleyland@gmail.com>
2025-12-21 15:11:39 -0800

    add new keybinding

--- lua/leyland/plugins/diffview.lua
+++ lua/leyland/plugins/diffview.lua
@@ -4,5 +4,6 @@ return {
 	keys = {
 		{ "<leader>dd", "<cmd>DiffviewOpen<cr>", desc = "Open diff view" },
 		{ "<leader>df", "<cmd>DiffviewClose<cr>", desc = "Close diff view" },
+		{ "<leader>ds", "<cmd>:DiffviewOpen origin/main...HEAD<cr>", desc = "Open branch diff view" },
 	},
 }
