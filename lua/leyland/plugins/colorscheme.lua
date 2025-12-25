5f3c5b665a47bcfe828b3a456bcdab017ec58a19
Leyland Yang <yangleyland@gmail.com>
2025-12-24 16:51:47 -0800

    Add bamboo theme, lazy sync

--- lua/leyland/plugins/colorscheme.lua
+++ lua/leyland/plugins/colorscheme.lua
@@ -10,12 +10,31 @@ return {
 		priority = 1000,
 	},
 	{
+		"miikanissi/modus-themes.nvim",
+		priority = 1000,
+	},
+	{
+		"ribru17/bamboo.nvim",
+		lazy = false,
+		priority = 1000,
+	},
+	{
+		"navarasu/onedark.nvim",
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			require("onedark").setup({
+				style = "deep",
+			})
+			require("onedark").load()
+		end,
+	},
+	{
 		"rose-pine/neovim",
 		name = "rose-pine",
 		priority = 1000,
 		config = function()
 			-- Set your default theme here
-			vim.cmd.colorscheme("catppuccin-mocha")
+			vim.cmd.colorscheme("bamboo")
 		end,
 	},
 }
---
+++ lua/leyland/plugins/trouble.lua
@@ -0,0 +1,8 @@
+return {
+	"folke/trouble.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {},
+	keys = {
+		{ "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Toggle Trouble" },
+	},
+}
