5b9269dfcafc25adc6afb5e1c612ac074ecf8ac6
Leyland Yang <yangleyland@gmail.com>
2025-12-24 17:36:06 -0800

    Add treesitter

--- lua/leyland/plugins/colorscheme.lua
+++ lua/leyland/plugins/colorscheme.lua
@@ -21,12 +21,12 @@ return {
 	{
 		"navarasu/onedark.nvim",
 		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			require("onedark").setup({
-				style = "deep",
-			})
-			require("onedark").load()
-		end,
+		-- config = function()
+		-- 	require("onedark").setup({
+		-- 		style = "deep",
+		-- 	})
+		-- 	require("onedark").load()
+		-- end,
 	},
 	{
 		"rose-pine/neovim",
