ea2a19fbb391346f6f143eb956ef5aa63c861b37
Leyland Yang <yangleyland@gmail.com>
2026-07-15 16:55:20 -0700

    change formatter

--- lua/leyland/plugins/formatting.lua
+++ lua/leyland/plugins/formatting.lua
@@ -3,19 +3,21 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
 		local conform = require("conform")
+		local js_formatter = { "oxfmt", "prettier", stop_after_first = true }
 
 		conform.setup({
 			formatters_by_ft = {
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescriptreact = { "prettier" },
+				javascript = js_formatter,
+				typescript = js_formatter,
+				javascriptreact = js_formatter,
+				typescriptreact = js_formatter,
 				svelte = { "prettier" },
-				css = { "prettier" },
+				css = js_formatter,
 				html = { "prettier" },
 				json = { "prettier" },
 				yaml = { "prettier" },
-				markdown = { "prettier" },
+				markdown = js_formatter,
+				["markdown.mdx"] = js_formatter,
 				graphql = { "prettier" },
 				liquid = { "prettier" },
 				lua = { "stylua" },
